624865a9ee4c615befc5b84f88147abd51293302
Salhsynva <6mgc0gt@code.edu.az>
2022-05-27 16:03:53 +0400

    first commit

---
+++ asp_classwork1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using asp_classwork1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_classwork1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ asp_classwork1/Models/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_classwork1.Models
+{
+    public class Card
+    {
+        public Card(int id, string name,string description,  string image, double price)
+        {
+            this.Id = id;
+            this.Image = image;
+            this.Price = price;
+            this.Description = description;
+            this.Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+    }
+}
